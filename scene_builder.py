54433c3956a42556cdde54f3d66064f1e909f4ff
griffincampbell6 <griffincampbell6@gmail.com>
2024-02-27 23:00:26 -0500

    add Panda3D script

---
+++ scene_builder.py
@@ -0,0 +1,11 @@
+from direct.showbase.ShowBase import ShowBase
+
+class StellarHorizon(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+
+app = StellarHorizon()
+app.run()
+
